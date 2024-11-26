78d1b83a5d5f7e207499ce83ab83b5f19459e560
Maksym Tsybulskyi <tomahawksmail@gmail.com>
2024-11-26 12:53:32 -0800

    docker-compose

--- renew.sh
+++ renew.sh
@@ -1,17 +1,4 @@
 
-
-docker build -t helpdesk-image:3.5.1 .
-docker network create --driver overlay helpdesk-network
-docker tag helpdesk-image:3.5.1 dregistrygui.uskoinc.com/helpdesk-image:3.5.1
-docker push dregistrygui.uskoinc.com/helpdesk-image:3.5.1
-
-docker stack deploy -c docker-compose.yml helpdesk_stack --with-registry-auth
-
-
-
-
-
-
 docker network create --driver overlay helpdesk-network
 docker tag brunoah/hesk dregistrygui.uskoinc.com/brunoah/hesk
 docker push dregistrygui.uskoinc.com/brunoah/hesk
@@ -19,8 +6,7 @@ docker push dregistrygui.uskoinc.com/brunoah/hesk
 docker service create --name helpdesk-service\
                       --with-registry-auth\
                       --replicas 5 \
-                      --publish published=9000,target=80,mode=ingress \
+                      --publish published=5544,target=80 \
                       dregistrygui.uskoinc.com/brunoah/hesk:latest || { echo "Service creation failed"; exit 1; }
 
 
-docker stack deploy -c docker-compose.yml helpdesk_stack --with-registry-auth
