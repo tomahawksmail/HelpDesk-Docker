ec3ed1361c9c76467084d4c0b29aa76e430db1b3
Maksym Tsybulskyi <tomahawksmail@gmail.com>
2024-11-26 15:45:10 -0800

    docker-compose

--- renew.sh
+++ renew.sh
@@ -3,11 +3,11 @@
 #docker tag brunoah/hesk dregistrygui.uskoinc.com/brunoah/hesk
 #docker push dregistrygui.uskoinc.com/brunoah/hesk
 #
-#docker service create --name helpdesk-service\
-#                      --with-registry-auth\
-#                      --replicas 5 \
-#                      --publish published=5544,target=80 \
-#                      dregistrygui.uskoinc.com/brunoah/hesk:latest || { echo "Service creation failed"; exit 1; }
+docker service create --name helpdesk-service\
+                      --with-registry-auth\
+                      --replicas 1 \
+                      --publish published=5544,target=80 \
+                      dregistrygui.uskoinc.com/helpdesk:3.5.1 || { echo "Service creation failed"; exit 1; }
 
 
  docker stack deploy -c docker-compose.yml helpdesk_stack --with-registry-auth
