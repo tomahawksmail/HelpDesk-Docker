cc34730ee01901ce5026ae61e3001bda28343192
Maksym Tsybulskyi <tomahawksmail@gmail.com>
2025-01-17 10:48:39 -0800

    docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
-FROM php:apache
-RUN apt-get update && docker-php-ext-install mysqli && apt-get install -y iputils-ping
-WORKDIR /var/www/html/
-COPY ./php.custom.ini /usr/local/etc/php/conf.d/php.custom.ini
-EXPOSE 80 3306
+FROM php:fpm
+LABEL maintainer="Luke Tainton <luke@tainton.uk>"
+LABEL org.opencontainers.image.source="https://github.com/luketainton/hesk-docker"
+
+# Set up PHP extensions
+RUN apt-get update && \
+    apt-get install -y libc-client-dev libkrb5-dev --no-install-recommends && \
+    docker-php-ext-configure imap --with-kerberos --with-imap-ssl && \
+    docker-php-ext-install mysqli imap && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+# Use the NFS share as the app directory
+WORKDIR /srv
--- renew.sh
+++ renew.sh
@@ -3,13 +3,12 @@
 docker image build -t helpdesk:3.5.1 .
 docker tag helpdesk:3.5.1 dregistrygui.uskoinc.com/helpdesk:3.5.1
 docker push dregistrygui.uskoinc.com/helpdesk:3.5.1
-#docker push dregistrygui.uskoinc.com/brunoah/hesk
-#
-docker service create --name helpdesk-service\
-                      --with-registry-auth\
-                      --replicas 1 \
-                      --publish published=5544,target=80 \
-                      helpdesk:latest || { echo "Service creation failed"; exit 1; }
+
+#docker service create --name helpdesk-service\
+#                      --with-registry-auth\
+#                      --replicas 1 \
+#                      --publish published=5544,target=80 \
+#                      helpdesk:latest || { echo "Service creation failed"; exit 1; }
 
 
 docker stack deploy -c docker-compose.yml helpdesk_stack --with-registry-auth
