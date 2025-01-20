5c46ec4335de47a106cea5e83a8d572770761add
Maksym Tsybulskyi <tomahawksmail@gmail.com>
2025-01-20 12:21:07 -0800

    docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@
 FROM php:8.2-apache
 
 # Install PDO MySQL extension
+
 RUN docker-php-ext-install pdo pdo_mysql mysqli
+
 
 # Set the working directory
 WORKDIR /var/www/html
--- renew.sh
+++ renew.sh
@@ -1,14 +1,17 @@
-
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
 #docker network create --driver overlay helpdesk-network
 docker image build -t helpdesk:3.5.2 .
 docker tag helpdesk:3.5.2 dregistrygui.uskoinc.com/helpdesk:3.5.2
 docker push dregistrygui.uskoinc.com/helpdesk:3.5.2
 
-#docker service create --name helpdesk-service\
-#                      --with-registry-auth\
-#                      --replicas 1 \
-#                      --publish published=5544,target=80 \
-#                      helpdesk:3.5.1 || { echo "Service creation failed"; exit 1; }
+docker service create --name helpdesk-service\
+                      --with-registry-auth\
+                      --replicas 4 \
+                      --network hesk-net \
+                      --mount type=bind,source=/mnt/nfs/shared/helpdesk/app,target=/var/www/html \
+                      --publish published=5545,target=80 \
+                      dregistrygui.uskoinc.com/helpdesk:3.5.2 || { echo "Service creation failed"; exit 1; }
 
+# docker builder prune
 
 docker stack deploy -c docker-compose.yml helpdesk_stack --with-registry-auth
